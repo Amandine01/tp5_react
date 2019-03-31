26dfe3c3f7333626514bb98d9e70b89dddda22db
Amandine01 <amandine.ducruet@free.fr>
2019-04-01 01:22:56 +0200

    Update AlbumGrid.js

--- src/container/AlbumGrid.js
+++ src/container/AlbumGrid.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Album from '../component/Album';
 
+import { Container, Row, Col } from 'reactstrap';
+
 export default class AlbumGrid extends Component {
   render() {
     return (
@@ -12,66 +14,97 @@ export default class AlbumGrid extends Component {
           </a>
         </div>
         <section id="albums">
-          <Album
-            image="img/album/album_01.png"
-            nom="Native (Deluxe)"
-            artist="OneRepublic"
-            song={17}
-          />
-          <Album
-            image="img/album/album_02.png"
-            nom="Wax Wings"
-            artist="Joshua Radin"
-            song={11}
-          />
-          <Album
-            image="img/album/album_03.png"
-            nom="Wax Modern Vampires of th..."
-            artist="Vampire Weekend"
-            song={12}
-          />
-          <Album
-            image="img/album/album_04.png"
-            nom="Night Visions"
-            artist="Imagine Dragons"
-            song={11}
-          />
-          <Album
-            image="img/album/album_05.png"
-            nom="Trouble Will Find Me"
-            artist="The National"
-            song={13}
-          />
-          <Album
-            image="img/album/album_06.png"
-            nom="Native (Deluxe)"
-            artist="OneRepublic"
-            song={17}
-          />
-          <Album
-            image="img/album/album_07.png"
-            nom="Wax Wings"
-            artist="Joshua Radin"
-            song={11}
-          />
-          <Album
-            image="img/album/album_08.png"
-            nom="Modern Vampires of th..."
-            artist="Vampire Weekend"
-            song={12}
-          />
-          <Album
-            image="img/album/album_09.png"
-            nom="Night Visions"
-            artist="Imagine Dragons"
-            song={11}
-          />
-          <Album
-            image="img/album/album_10.png"
-            nom="Trouble Will Find Me"
-            artist="The National"
-            song={13}
-          />
+          <Row>
+        	 <Col xs="6" sm="6" md="3" lg="2">
+             <Album
+                image="img/album/album_01.png"
+                nom="Native (Deluxe)"
+                artist="OneRepublic"
+                song={17}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_02.png"
+                nom="Wax Wings"
+                artist="Joshua Radin"
+                song={11}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_03.png"
+                nom="Wax Modern Vampires of th..."
+                artist="Vampire Weekend"
+                song={12}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_04.png"
+                nom="Night Visions"
+                artist="Imagine Dragons"
+                song={11}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_05.png"
+                nom="Trouble Will Find Me"
+                artist="The National"
+                song={13}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_06.png"
+                nom="Native (Deluxe)"
+                artist="OneRepublic"
+                song={17}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_07.png"
+                nom="Wax Wings"
+                artist="Joshua Radin"
+                song={11}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_08.png"
+                nom="Modern Vampires of th..."
+                artist="Vampire Weekend"
+                song={12}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_09.png"
+                nom="Night Visions"
+                artist="Imagine Dragons"
+                song={11}
+              />
+            </Col>
+
+            <Col xs="6" sm="6" md="3" lg="2">
+              <Album
+                image="img/album/album_10.png"
+                nom="Trouble Will Find Me"
+                artist="The National"
+                song={13}
+              />
+            </Col>
+          </Row>
         </section>
       </div>
     );
